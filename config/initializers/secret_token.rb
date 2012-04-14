f2e1e7a43fbbb4baf0689e387a508beacb5084b1
Alshaimaa <alshaimaa@alshaimaa-Inspiron-1464.(none)>
2012-04-14 17:50:21 +0200

    empty rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Forsa::Application.config.secret_token = '2e603d774e6a45956704be28d43d26667f1aa0f7b80008d59dd6d3573d95d08cd8f78ea57b295725762d360bfe166ca99e8e7b969e6066610f0f443e95d89241'
